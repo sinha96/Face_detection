3da033eaabad440e3bf61492ce02cf1ad0b69194
Priyanshu Shekhar Sinha <priyanshu1996@hotmail.com>
2019-01-25 02:16:39 +0530

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -10,14 +10,17 @@ def detect(gray, frame): # We create a function that takes as input the image in
     faces = face_cascade.detectMultiScale(gray, 1.3, 5) # We apply the detectMultiScale method from the face cascade to locate one or several faces in the image.
     for (x, y, w, h) in faces: # For each detected face:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) # We paint a rectangle around the face.
+        cv2.putText(frame, "face", (x, y-5), font, 0.7, (225, 225, 51), 1, cv2.LINE_AA)
         roi_gray = gray[y:y+h, x:x+w] # We get the region of interest in the black and white image.
-        roi_color = frame[y:y+h, x:x+w] # We get the region of interest in the colored image.
+        roi_color = frame[y:y+h, x:x+w] # We get the region of interest in the colored image.        
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22) # We apply the detectMultiScale method to locate one or several eyes in the image.
         for (ex, ey, ew, eh) in eyes: # For each detected eye:
             cv2.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh), (0, 255, 0), 2) # We paint a rectangle around the eyes, but inside the referential of the face.
+            cv2.putText(roi_color, "eye", (ex, ey-5), font, 0.7, (225, 225, 51), 1, cv2.LINE_AA)
         smile = smile_cascade.detectMultiScale(roi_gray, 1.7, 22) # We apply the detectMultiScale method to locate one or several smiles in the image.
         for (sx, sy, sw, sh) in eyes: # For each detected eye:
             cv2.rectangle(roi_color,(sx, sy),(sx+sw, sy+sh), (0, 0, 255), 2) # We paint a rectangle around the eyes, but inside the referential of the face.
+            cv2.putText(roi_color, "smile", (sx, sy-5), font, 0.7, (225, 225, 51), 1, cv2.LINE_AA)
     return frame # We return the image with the detector rectangles.
 
 video_capture = cv2.VideoCapture(0) # We turn the webcam on.
