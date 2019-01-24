7e4794b250b1f426ea8d97d6573f376f6a77ac2b
Priyanshu Shekhar Sinha <priyanshu1996@hotmail.com>
2019-01-25 01:53:07 +0530

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -4,6 +4,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') # We load the cascade for the face.
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') # We load the cascade for the eyes.
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml') # We load the cascade for the eyes.
 
 def detect(gray, frame): # We create a function that takes as input the image in black and white (gray) and the original image (frame), and that will return the same image with the detector rectangles. 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5) # We apply the detectMultiScale method from the face cascade to locate one or several faces in the image.
@@ -11,9 +12,12 @@ def detect(gray, frame): # We create a function that takes as input the image in
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) # We paint a rectangle around the face.
         roi_gray = gray[y:y+h, x:x+w] # We get the region of interest in the black and white image.
         roi_color = frame[y:y+h, x:x+w] # We get the region of interest in the colored image.
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3) # We apply the detectMultiScale method to locate one or several eyes in the image.
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22) # We apply the detectMultiScale method to locate one or several eyes in the image.
         for (ex, ey, ew, eh) in eyes: # For each detected eye:
             cv2.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh), (0, 255, 0), 2) # We paint a rectangle around the eyes, but inside the referential of the face.
+        smile = smile_cascade.detectMultiScale(roi_gray, 1.7, 22) # We apply the detectMultiScale method to locate one or several smiles in the image.
+        for (sx, sy, sw, sh) in eyes: # For each detected eye:
+            cv2.rectangle(roi_color,(sx, sy),(sx+sw, sy+sh), (0, 0, 255), 2) # We paint a rectangle around the eyes, but inside the referential of the face.
     return frame # We return the image with the detector rectangles.
 
 video_capture = cv2.VideoCapture(0) # We turn the webcam on.
